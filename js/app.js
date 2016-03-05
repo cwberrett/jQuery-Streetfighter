5d468320bc51e3e7878a1ee0ae134f39c1f83a89
CWBerrett <cwberrett@gmail.com>
2016-03-05 13:49:53 -0800

    Added hadouken animation and sound

--- js/app.js
+++ js/app.js
@@ -8,9 +8,16 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})//ryu relaxes on mouseleave
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left','520px');
+			});
 		//play sound
 		//show throw
 	})//fire hadouken
@@ -19,3 +26,8 @@ $(document).ready(function(){
 		$('.ryu-ready').show();
 	})//return to ready position
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
