a8fbabd3ebd51e7484ce760c2fcbb3c314e78f19
CWBerrett <cwberrett@gmail.com>
2016-03-05 18:15:35 -0800

    Added Cool functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
+	$(".ryu-action").hide();
+	$(".ryu-still").show();  //default state
+
 	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
+		$(".ryu-action").hide();
 		$('.ryu-ready').show();
 	})//Ryu gets ready on mouseover
 	.mouseleave(function() {
-		$('.ryu-ready').hide();
+		$(".ryu-action").hide();
 		$('.ryu-still').show();
 	})//ryu relaxes on mouseleave
 	.mousedown(function() {
 		playHadouken();
-		$('.ryu-ready').hide();
+		$(".ryu-action").hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left':'1020px'},
@@ -18,16 +21,37 @@ $(document).ready(function(){
 				$(this).hide();
 				$(this).css('left','520px');
 			});
-		//play sound
-		//show throw
 	})//fire hadouken
 	.mouseup(function() {
-		$('.ryu-throwing').hide();
+		$(".ryu-action").hide();
 		$('.ryu-ready').show();
-	})//return to ready position
+	});//return to ready position
+	$(document).keydown(function(x) {
+		if (x.keyCode == 88) {
+			playcool();
+			$('.ryu-action').hide();
+        	$('.ryu-cool').show();
+		}
+	});
+	$(document).keyup(function(x){
+		if(x.keyCode == 88) {
+			$('.ryu-action').hide();
+        	$('.ryu-still').show();
+        	$('#cool-sound')[0].pause();
+		}
+	});
 });
+var hadoukenSound = false;
 function playHadouken () {
+	hadoukenSound = !hadoukenSound
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
+}
+var coolSound = false;
+function playcool () {
+	coolSound = !coolSound;
+  //$('#cool-sound')[0].volume = 0.5;
+  //$('#hadouken-sound')[0].load();
+  $('#cool-sound')[0].play();
 }
